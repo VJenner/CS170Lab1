ab9d5eb07e8b559ee6d2c9ce6a672d6bdd454ece
Victoria Jenner <vjenner@mills.edu>
2013-01-31 11:42:23 -0800

    testing github new commit

--- Lab1.rb
+++ Lab1.rb
@@ -28,3 +28,4 @@ my_num = 1
 
 # 7. Create at least one more object of the same type, and create an array of these objects. Iterate through the array and 
 # print to the console one field, such as thre first name. 
+#any change
